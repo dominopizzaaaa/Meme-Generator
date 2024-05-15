5bdaaa10be127e19b563079ea690029165a26ef5
dominopizzaaaa <94787443+dominopizzaaaa@users.noreply.github.com>
2024-05-15 16:02:49 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import React from "react"
+import Header from './Header'
+import Meme from './Meme'
+
+
+export default function App() {
+  return (
+      <div>
+        <Header />
+        <Meme />
+      </div>
+  )
+}
